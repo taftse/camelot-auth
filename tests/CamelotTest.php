ce566e1fa42f15da846b1d8a9e46efad08b8f993
Timothy Seebus <support@twsweb-int.com>
2013-06-29 21:36:26 +0100

    first tests

---
+++ tests/CamelotTest.php
@@ -0,0 +1,21 @@
+<?php namespace T4s\CamelotAuth\Tests;
+
+use Mockery as m;
+use T4s\CamelotAuth\Camelot;
+use PHPUnit_Framework_TestCase;
+
+class CamelotTest extends PHPUnit_Framework_TestCase
+{
+	protected $session;
+
+	protected $cookie;
+
+	public function setUp()
+	{
+		$this->camelot = new Camelot(
+			$this->session = m::mock('T4s\CamelotAuth\Session\SessionInterface'),
+			$this->cookie = m::mock('T4s\CamelotAuth\Cookie\CookieInterface'),
+			);
+	}
+}
+
---
+++ tests/Cookie/illuminateCookieTest.php
@@ -0,0 +1,35 @@
+<?php namespace T4s\CamelotAuth\Tests\Cookie;
+
+use Mockery as m;
+use T4s\CamelotAuth\Cookie\IlluminateCookie;
+use PHPUnit_Framework_TestCase;
+
+class IlluminateCookieTest extends PHPUnit_Framework_TestCase
+{
+	protected $cookieJar
+
+	protected $cookie;
+
+	public function setUp()
+	{
+		$this->cookieJar = m::mock('Illuminate\Cookie\CookieJar');
+		$cookie = new IlluminateCookie($this->cookieJar,"camelot-auth-cookie");
+	}
+
+	public function tearDown()
+	{
+		m::close();
+	}
+
+	public function testPut()
+	{
+		$this->cookieJar->shouldReceive('make')->with('camelot-auth-cookie','cookie','123')->once();
+		$this->cookie->put('cookie','123');
+	}
+
+	public function testForever()
+	{
+		$this->cookieJar->shouldReceive('forever','cookie')->once();
+		$this->cookie->forever('cookie');
+	}
+}
