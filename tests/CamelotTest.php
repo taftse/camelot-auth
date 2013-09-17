9b7de2461b754e661cc3cd0fcd54fb8b34a24bf4
Ferdinand Leinbach <ferdi.leinbach@googlemail.com>
2013-09-17 23:54:31 +0200

    made CamelotTest setUp() work again

--- tests/CamelotTest.php
+++ tests/CamelotTest.php
@@ -3,20 +3,37 @@
 use Mockery as m;
 use T4s\CamelotAuth\Camelot;
 use PHPUnit_Framework_TestCase;
+use Hamcrest;
 
 class CamelotTest extends PHPUnit_Framework_TestCase
 {
 	protected $session;
 
 	protected $cookie;
+	
+	protected $camelot;
+	
+	protected $config;
+	
+	protected $messaging;
+	
+	protected $path;
 
 	public function setUp()
 	{
-		$this->camelot = new Camelot(
-			$this->session = m::mock('T4s\CamelotAuth\Session\SessionInterface'),
-			$this->cookie = m::mock('T4s\CamelotAuth\Cookie\CookieInterface'),
-			$this->config = m::mock('T4s\CamelotAuth\Config\ConfigInterface')
-			);
+		$this->session = m::mock('T4s\CamelotAuth\Session\SessionInterface');
+		$this->cookie = m::mock('T4s\CamelotAuth\Cookie\CookieInterface');
+		$this->config = m::mock('T4s\CamelotAuth\Config\ConfigInterface');
+		$this->messaging = m::mock('T4s\CamelotAuth\Messaging\MessagingInterface');
+		$this->path = m::mock(\stringValue());
+		
+		$this->config->shouldReceive('get')->once()->with('camelot.provider_routing')
+					 ->andReturn('foo');
+		$this->config->shouldReceive('get')->once()->with('camelot.database_driver')
+					 ->andReturn('Eloquent');
+		
+		$this->camelot = new Camelot($this->session,$this->cookie,$this->config,$this->messaging,$this->path);
+		
 	}
 
 	public function tearDown()
@@ -27,6 +44,11 @@ class CamelotTest extends PHPUnit_Framework_TestCase
 	public function getEventDispacherTest()
 	{
 		
+	}
+	
+	public function testSetEventDispatcher() 
+	{
+		$dispatcher = m::mock('T4s\CamelotAuth\Events\DispatcherInterface');
 	}
 }
 
