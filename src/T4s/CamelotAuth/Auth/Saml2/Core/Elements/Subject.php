0994a8acbf17d9d601daef9f150f6ef8b03f1027
Timothy Seebus <timothyseebus@tools4schools.org>
2014-07-06 01:15:28 +0100

    next node

--- src/T4s/CamelotAuth/Auth/Saml2/Core/Elements/Subject.php
+++ src/T4s/CamelotAuth/Auth/Saml2/Core/Elements/Subject.php
@@ -11,6 +11,7 @@ namespace T4s\CamelotAuth\Auth\Saml2\Core\Elements;
 
 
 use T4s\CamelotAuth\Auth\Saml2\Metadata\Elements\SAMLElementInterface;
+use T4s\CamelotAuth\Auth\Saml2\Saml2Constants;
 
 class Subject implements SAMLElementInterface
 {
@@ -27,5 +28,34 @@ class Subject implements SAMLElementInterface
     public function addSubjectConfirmation(SubjectConfirmation $subjectConfirmation)
     {
         $this->subjectConfirmation[] = $subjectConfirmation;
+    }
+
+    public function  toXML(\DOMElement $parentElement)
+    {
+        $subject = $parentElement->ownerDocument->createElementNS(Saml2Constants::Namespace_SAML, 'saml:Subject');
+        $parentElement->appendChild($subject);
+
+        if(!is_null($this->baseID))
+        {
+            foreach($this->baseID as $baseID)
+            {
+                $baseID->toXML($subject);
+            }
+        }
+
+        if(!is_null($this->subjectConfirmation))
+        {
+            foreach ($this->subjectConfirmation as $subjectConfirmation )
+            {
+                $subject->toXML($subject);
+            }
+        }
+
+        return $subject;
+    }
+
+    public function importXML(\DOMElement $node)
+    {
+        throw new \Exception('unfinished function Subject->importXML()');
     }
 } 
