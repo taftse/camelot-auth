8ba27df3392bff325535fe8f2fbbcf822c3799e4
Timothy Seebus <Timothyseebus@tools4schools.org>
2015-05-10 16:10:17 +0100

    add a laravel service provider

---
+++ src/T4s/CamelotAuth/CamelotServiceProvider.php
@@ -0,0 +1,35 @@
+<?php namespace SE\Providers;
+
+use Illuminate\Support\ServiceProvider;
+use T4s\CamelotAuth\src\T4s\CamelotAuth\CamelotManager;
+
+class CamelotServiceProvider extends ServiceProvider {
+
+	/**
+	 * Bootstrap the application services.
+	 *
+	 * @return void
+	 */
+	public function boot()
+	{
+		//
+	}
+
+	/**
+	 * Register the application services.
+	 *
+	 * @return void
+	 */
+	public function register()
+	{
+		$this->app->singleton('camelot', function($app)
+        {
+            // Once the authentication service has actually been requested by the developer
+            // we will set a variable in the application indicating such. This helps us
+            // know that we need to set any queued cookies in the after event later.
+            $app['camelot.loaded'] = true;
+            return new CamelotManager($app['request']->path());
+        });
+	}
+
+}
